2de30f7e8b203ff70dd36188f653704224706144
Shivabhandari-7 <b018468l@student.staffs.ac.uk>
2023-07-10 13:51:20 +0100

    Program6_Area_of_a_Rectangle

---
+++ Weak 1/Program6_Area_of_a_Rectangle/Program6_Area_of_a_Rectangle/Program6_Area_of_a_Rectangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Create variables for width and height
+    int width = 15;
+    int height = 25;
+
+    // Calculate the area
+    int area = width * height;
+
+    // Output the result
+    cout << "The area of a " << width << " by " << height << " rectangle is " << area << endl;
+
+    return 0;
+}
